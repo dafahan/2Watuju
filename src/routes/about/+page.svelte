7048a98f11141fb89afbeaa7ff62225e49e4364c
dafahan <dafahanid@gmail.com>
2025-07-10 15:07:04 +0700

    feat:optimizing , and create proses kerja section

--- src/routes/about/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Testimonials from "$lib/components/Testimonials.svelte";
-
-</script>
-  
-<div class="flex h-96 w-full bg-red-500"></div>
-  <Testimonials />    
-  
-<div class="flex h-96 w-full bg-red-500"></div>
