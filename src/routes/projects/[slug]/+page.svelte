589e99f1b2070e3e80b6ece617ba024042e6a39d
dafahan <dafahanid@gmail.com>
2025-07-31 11:52:54 +0700

    feat:panoramic

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -243,7 +243,7 @@
     <div class="flex w-full border-b-2 border-gray-200 py-12">
         <div class="flex w-full py-8 flex-col gap-4" >
           <img src={project.images.desktop} alt="thumb" class="object-cover flex aspect-video w-full shadow-lg rounded-sm mb-12"/>
-          <h1 class="font-bold font-roboto-mono text-xl lg:text-3xl xl:text-5xl">Fasad Yang Mencerminkan Kehangatan</h1>
+          <h1 class="font-bold font-roboto-mono text-xl lg:text-2xl xl:text-3xl">Fasad Yang Mencerminkan Kehangatan</h1>
 
           <p class="text-pretty text-sm lg:text-base xl:text-xl font-roboto">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
